b73373eb23bae1227c2dcb870b551a85df3fa4de
houssem121 <hammami290@gmail.com>
2024-04-07 03:47:28 +0100

    up

--- client/src/patientservice/registercomp.js
+++ client/src/patientservice/registercomp.js
@@ -2,7 +2,7 @@ import { Card, Form, Button, Message } from 'semantic-ui-react';
 import React from "react";
 import { useState } from 'react';
 import {ContractPatient} from '../Contract';
-import { useEffect } from 'react';
+import { useEffect } from 'react';//this is a comment
     const RegisterComp = (props) => {
         const [formData, setFormData] = useState({
             firstname: '',
